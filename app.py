623016ccf6d3618b06e9f70134766157c4da1950
Kay Gijzen <kaygijzen98@gmail.com>
2021-11-02 21:04:41 +0100

    first version

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import random
+
+# TODO: expand the chatbot
 
 app = Flask(__name__)
 
@@ -13,7 +16,7 @@ trainer.train("chatterbot.corpus.english")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", id=random.randint(1, 1000))
 
 
 @app.route("/get")
